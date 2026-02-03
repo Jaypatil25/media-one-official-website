57b47126b2aed3d59cf9e391401e26a8d7ba4cf2
Jay <jaypatil.nst@gmail.com>
2026-02-03 23:54:11 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Commit date: January 30, 2025
+// Version: 1.0.0 - Multi-language support and smooth UI enhancements
+
 createRoot(document.getElementById("root")!).render(<App />);
